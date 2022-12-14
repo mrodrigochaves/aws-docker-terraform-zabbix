382e751ab2f7c12618b4518853d01b22ffb8470d
Márcio Rodrigo <39975073+mrodrigochaves@users.noreply.github.com>
2022-12-14 10:55:51 -0300

    Finalizando Terraform

--- main.tf
+++ main.tf
@@ -1,4 +1,3 @@
-
 # Provisionando VPC
 resource "aws_vpc" "zabbix-vpc" {
   cidr_block = "10.0.0.0/16"
@@ -59,26 +58,30 @@ variable "key_name" {
     default = "sga-key"
 }
 
-# Provisionando Instância
+locals {
+  vpc_id	= "..."
+  subnet_id	= "..."
+}
+
 resource "aws_instance" "zabbix-sv" {
   ami           = "ami-0a6b2839d44d781b2"
   instance_type = "t2.micro"
-  key_name = var.key_name
-  vpc_security_group_ids = [aws_security_group.zabbix-server-sg.id]
+  subnet_id	= aws_subnet.zabbix-public-subnet.id
+  vpc_security_group_ids = ["${aws_security_group.zabbix-server-sg.id}"]
   associate_public_ip_address = true
-
-  user_data = <<-EOF
+ 
+ user_data = <<-EOF
               #!/bin/bash
               echo "Olá, Terraform & AWS" > index.html
               nohup busybox httpd -f -p "${var.http_port}" &
               EOF
+
   tags = {
     Name = "zabbix-sv"
     ambiente = "zabbix"
   }
 }
 
-# Provisionando Security Group
 resource "aws_security_group" "zabbix-server-sg" {
   name        = "zabbix-server-sg"
   description = "Allow SSH and HTTP traffic on EC2 instance"
@@ -91,6 +94,7 @@ resource "aws_security_group" "zabbix-server-sg" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
   ingress {
     description = "HTTP to EC2"
     from_port   = var.http_port
@@ -98,12 +102,14 @@ resource "aws_security_group" "zabbix-server-sg" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
   egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
   tags = {
     Name = "zabbix-server-sg"
   }
@@ -117,4 +123,5 @@ output "public_ip" {
   value       = aws_instance.zabbix-sv.public_ip
   description = "The public IP of the web server"
 }
+
 

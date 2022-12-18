5ee787f7d910a5c86b4dac14c2b7af03447eefed
Márcio Rodrigo <dev.marciorodrigo@gmail.com>
2022-12-18 09:28:14 -0300

    modificando porta

--- docker-compose/docker-install.sh
+++ docker-compose/docker-install.sh
@@ -1,21 +1,34 @@
 #!/bin/bash
 # Instalação do Docker
-curl -fsSl  https://get.docker.com/| sh
 
-# Verificando versão do Docker
+apt install apt-transport-https ca-certificates curl software-properties-common
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+
+apt-cache policy docker-ce
+
+apt install docker-ce
+
 docker version
 
 # Instalação do Docker Compose
-curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
- 
-# Adicionando permissão de executável ao Docker Compose
+
+curl -L "https://github.com/docker/compose/releases/download/1.26.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+
 chmod +x /usr/local/bin/docker-compose
 
-# Criando atalho para o Docker Compose
-ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+docker-compose --version
 
-# Verificando versão do Docker Compose
-docker-compose version
+mkdir ~/docker-compose
 
-# Efetuando pull das imagens configuradas
+cd ~/docker-compose
+
+curl https://mr-aws-zabbix.s3.amazonaws.com/docker-compose.zip -O -J -L
+
+apt install unzip
+
+unzip docker-compose.zip
+
 docker-compose up -d
